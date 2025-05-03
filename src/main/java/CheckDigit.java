07905c72f12cc19a26ecbd1fe1fc588dbce5168c
MuhammadSiddiqui121 <Musiddiqui@s.sfusd.edu>
2025-05-02 23:12:25 -0700

    Update CheckDigit.java

--- src/main/java/CheckDigit.java
+++ src/main/java/CheckDigit.java
@@ -8,6 +8,10 @@ public class CheckDigit
    public static int getCheck(int num) 
    {  
      /* to be implemented in part (a) */
+     int sum =0;
+     for( int i = 1; i <= getNumberOfDigits(num); i++){
+       sum += (8 -i) * getDigit(num, i);
+   }
    }
  
   /** Returns true if numWithCheckDigit is valid, or false    
@@ -19,6 +23,7 @@ public class CheckDigit
    public static boolean isValid(int numWithCheckDigit)    
    {      
      /* to be implemented in part (b) */    
+     return ( getCheck(numWithCheckDigit/10) == numWithCheckDigit%10);
    }    
    
    /** Returns the number of digits in num. */    
